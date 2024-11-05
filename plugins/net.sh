fddaa0af8ddfd3cbc2b149361bc8f9a7d5da6f7c
Saxon Chen <sxa05z@gmail.com>
2024-11-05 12:01:17 +0800

    fix deprecated network command

--- plugins/net.sh
+++ plugins/net.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-LABEL=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==13 {print $2}')
+LABEL=$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')
 
 sketchybar --set $NAME label=$LABEL
